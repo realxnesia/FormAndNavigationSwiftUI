e37e0d606350428ddf867d5eebf90edf9bdbe0c8
dhikadityre <dhikaaditya1@gmail.com>
2021-07-28 20:48:44 +0700

    Initial Commit

---
+++ FormAndNavigation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FormAndNavigation
+//
+//  Created by Dhika Aditya Are on 28/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
